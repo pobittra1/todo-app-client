62a4436afb9c3283d75436e8ffa8caab041e217e
pobi <pobittrachandraroy2001@gmail.com>
2024-04-23 19:49:45 +0600

    implement login and register page with shadcn

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,49 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Link } from "react-router-dom";
 
-
-const Login = () => {
-    return (
-        <div>
-            <p>this is lofin route</p>
-        </div>
-    );
-};
-
-export default Login;
+export default function LoginForm() {
+  return (
+    <div className="w-full h-screen flex justify-center items-center bg-slate-500">
+      <Card className="w-full max-w-sm">
+        <CardHeader>
+          <CardTitle className="text-2xl">Login</CardTitle>
+          <CardDescription>
+            Enter your email below to login to your account.
+          </CardDescription>
+        </CardHeader>
+        <CardContent className="grid gap-4">
+          <div className="grid gap-2">
+            <Label htmlFor="email">Email</Label>
+            <Input
+              id="email"
+              type="email"
+              placeholder="m@example.com"
+              required
+            />
+          </div>
+          <div className="grid gap-2">
+            <Label htmlFor="password">Password</Label>
+            <Input id="password" type="password" required />
+          </div>
+        </CardContent>
+        <CardFooter>
+          <Link
+            to="/register"
+            className="w-full bg-black text-white py-2 px-4 rounded text-center"
+          >
+            Register
+          </Link>
+        </CardFooter>
+      </Card>
+    </div>
+  );
+}
